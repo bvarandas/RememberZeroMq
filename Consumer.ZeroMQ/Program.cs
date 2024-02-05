9938ef1d56eaede5912f0a36564785bb5a235c94
Bruno Varandas Ribeiro <b_varandas@LIVE.COM>
2024-02-05 15:03:31 -0300

    Testes While  True SpinWait

---
+++ Consumer.ZeroMQ/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetMQ;
+using NetMQ.Sockets;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Connecting to hello world server…");
+
+//using (var rep1 = new ResponseSocket("@tcp://localhost:5001"))
+//using (var rep2 = new ResponseSocket("@tcp://localhost:5002"))
+//using (var poller = new NetMQPoller { rep1, rep2 })
+//{
+//    // these event will be raised by the Poller
+//    rep1.ReceiveReady += (s, a) =>
+//    {
+//        // receive won't block as a message is ready
+//        string msg = a.Socket.ReceiveFrameString();
+//        // send a response
+//        a.Socket.SendFrame("Response");
+//    };
+//    rep2.ReceiveReady += (s, a) =>
+//    {
+//        // receive won't block as a message is ready
+//        string msg = a.Socket.ReceiveFrameString();
+//        // send a response
+//        a.Socket.SendFrame("Response");
+//    };
+//    // start polling (on this thread)
+//    poller.Run();
+//}
+
+using (var subscriber = new SubscriberSocket())
+{
+    subscriber.Connect("tcp://localhost:5555");
+    subscriber.Subscribe("A");
+
+    while (true)
+    {
+        var topic = subscriber.ReceiveFrameString();
+        var msg = subscriber.ReceiveFrameString();
+
+        Console.WriteLine("From Publisher: {0} {1}", topic, msg);
+    }
+}
+
+
+
+//using (var requester = new RequestSocket())
+//{
+//    requester.Connect("tcp://localhost:5555");
+
+//    int requestNumber;
+//    for (requestNumber = 0; requestNumber != 10; requestNumber++)
+//    {
+//        Console.WriteLine("Sending Hello {0}...", requestNumber);
+//        requester.SendFrame("Hello");
+//        string str = requester.ReceiveFrameString();
+//        Console.WriteLine("Received World {0}", requestNumber);
+//    }
+//}
---
+++ Producer.ZeroMQ/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NetMQ;
+using NetMQ.Sockets;
+
+Console.WriteLine("Hello, World!");
+
+//using (var responder = new ResponseSocket())
+//{
+//    responder.Bind("tcp://*:5555");
+
+//    while (true)
+//    {
+//        string str = responder.ReceiveFrameString();
+//        Console.WriteLine("Received Hello");
+//        Thread.Sleep(1000);  //  Do some 'work'
+//        responder.SendFrame("World");
+//    }
+//}
+
+//using (var rep = new ResponseSocket("@tcp://*:5002"))
+//{
+//    // process requests, forever...
+//    while (true)
+//    {
+//        // receive a request message
+//        var msg = rep.ReceiveFrameString();
+//        // send a canned response
+//        rep.SendFrame("Response");
+//    }
+//}
+
+using (var publisher = new PublisherSocket())
+{
+    publisher.Bind("tcp://*:5555");
+    int i = 0;
+    SpinWait spin = new SpinWait();
+    while (true)
+    {
+        publisher
+           .SendMoreFrame("A") // Topic
+           .SendFrame(i.ToString()); // Message
+
+        i++;
+
+        if ((i % 1000) == 0 )
+        //spin.SpinOnce(1);
+        Thread.Sleep(1);
+    }
+}
